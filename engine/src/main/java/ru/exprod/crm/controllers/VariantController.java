ebce2449d473a8d61054cfd63a58d0e3ad4ca0fd
Smehhh <e.qein666@gmail.com>
2021-02-20 20:05:08 +0300

    init comit

--- engine/src/main/java/ru/exprod/crm/controllers/VariantController.java
+++ engine/src/main/java/ru/exprod/crm/controllers/VariantController.java
@@ -1,8 +1,11 @@
 package ru.exprod.crm.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import ru.exprod.crm.controllers.model.SearchRequest;
 import ru.exprod.crm.controllers.model.Variant;
 
 import java.util.List;
@@ -12,8 +15,9 @@ import static java.util.Collections.emptyList;
 @RestController
 @RequestMapping("/api/variants")
 public class VariantController {
+
     @GetMapping
-    public List<Variant> getOrders() {
+    public List<Variant> getOrders(@RequestParam(name = "search", required = false) SearchRequest searchRequest) {
         return emptyList();
     }
 }
---
+++ engine/src/main/java/ru/exprod/crm/controllers/model/SearchRequest.java
@@ -0,0 +1,8 @@
+package ru.exprod.crm.controllers.model;
+
+
+public class SearchRequest {
+    String name;
+    String code;
+    String external_code;
+}
