0d871c31bac026b337499928b943056516ce5571
Smehhh <e.qein666@gmail.com>
2021-02-27 22:40:24 +0300

    create immutable variant

--- engine/src/main/java/ru/exprod/crm/controllers/VariantController.java
+++ engine/src/main/java/ru/exprod/crm/controllers/VariantController.java
@@ -6,12 +6,11 @@ import ru.exprod.crm.controllers.model.Variant;
 import ru.exprod.crm.service.model.VariantModel;
 import ru.exprod.crm.service.db.VariantService;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/api/variants")
+@RequestMapping("/api")
 public class VariantController {
 
     @Autowired
@@ -21,20 +20,6 @@ public class VariantController {
     public List<Variant> getOrders(@PathVariable int unit_id,
                                    @RequestParam(name = "search", required = false, defaultValue = "") String search) {
         List<VariantModel> models = variantService.searchWithFilter(unit_id, search);
-        return models.stream().map(this::createVariant).collect(Collectors.toList());
-    }
-
-    public Variant createVariant(VariantModel vm){
-        return new Variant()
-                .setId(vm.getId())
-                .setImageGroupId(vm.getImageGroupId())
-                .setImages(vm.getImages())
-                .setArchived(vm.getArchived())
-                .setCode(vm.getCode())
-                .setExternalCode(vm.getExternalCode())
-                .setName(vm.getName())
-                .setPrice(vm.getPrice())
-                .setQuantity(vm.getQuantity())
-                .setMoyskladId(vm.getMoyskladId());
+        return models.stream().map(Variant::new).collect(Collectors.toList());
     }
 }
--- engine/src/main/java/ru/exprod/crm/controllers/model/Variant.java
+++ engine/src/main/java/ru/exprod/crm/controllers/model/Variant.java
@@ -1,82 +1,46 @@
 package ru.exprod.crm.controllers.model;
 
 import ru.exprod.crm.service.model.ImageModel;
+import ru.exprod.crm.service.model.VariantModel;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 public class Variant {
-    private Integer id;
-    private String moyskladId;
-    private String name;
-    private String code;
-    private String externalCode;
-    private Boolean archived;
-    private BigDecimal price;
-    private BigDecimal quantity;
-    private Integer imageGroupId;
-    private List<ImageModel> images;
+    private final Integer id;
+    private final String moyskladId;
+    private final String name;
+    private final String code;
+    private final String externalCode;
+    private final Boolean archived;
+    private final BigDecimal price;
+    private final BigDecimal quantity;
+    private final Integer imageGroupId;
+    private final List<ImageModel> images;
 
-    public Variant setExternalCode(String externalCode) {
-        this.externalCode = externalCode;
-        return this;
-    }
-
-    public Variant setArchived(Boolean archived) {
-        this.archived = archived;
-        return this;
-    }
-
-    public Variant setPrice(BigDecimal price) {
-        this.price = price;
-        return this;
-    }
-
-    public Variant setQuantity(BigDecimal quantity) {
-        this.quantity = quantity;
-        return this;
-    }
-
-    public Variant setImageGroupId(Integer imageGroupId) {
-        this.imageGroupId = imageGroupId;
-        return this;
-    }
-
-    public Variant setImages(List<ImageModel> images) {
-        this.images = images;
-        return this;
-    }
-
-    public Variant setCode(String code) {
-        this.code = code;
-        return this;
+    public Variant(VariantModel vm){
+        this.id = vm.getId();
+        this.moyskladId = vm.getMoyskladId();
+        this.name = vm.getName();
+        this.code = vm.getCode();
+        this.externalCode = vm.getExternalCode();
+        this.archived = vm.getArchived();
+        this.price = vm.getPrice();
+        this.quantity = vm.getQuantity();
+        this.imageGroupId = vm.getImageGroupId();
+        this.images = vm.getImages();
     }
 
     public Integer getId() {
         return id;
     }
 
-    public Variant setId(Integer id) {
-        this.id = id;
-        return this;
-    }
-
     public String getMoyskladId() {
         return moyskladId;
     }
 
-    public Variant setMoyskladId(String moyskladId) {
-        this.moyskladId = moyskladId;
-        return this;
-    }
-
     public String getName() {
         return name;
-    }
-
-    public Variant setName(String name) {
-        this.name = name;
-        return this;
     }
 
     public String getCode() {
